c237c9a608183e16b5beca3cd2576769dfd5adb5
takux <53621441+takux@users.noreply.github.com>
2022-06-29 15:02:55 +0900

    Update

--- src/components/NotionBlock.tsx
+++ src/components/NotionBlock.tsx
@@ -37,7 +37,6 @@ const NotionBlock: FC<BlockProps> = ({
 };
 
 const NotionBlockCore: FC<BlockProps> = ({ block }) => {
-  console.log("呼ばれました", 999);
   const { prefix } = useContext(Context);
   switch (block.type) {
     case BlockEnum.paragraph:

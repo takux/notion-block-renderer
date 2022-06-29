2ef98d3a24219de552ac33568b1f4f1cf46d55fa
takux <53621441+takux@users.noreply.github.com>
2022-06-30 00:29:39 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // import "./styles.css";
+import NotionBlocks from "./components/NotionBlocks";
 import NotionBlock from "./components/NotionBlock";
 
 export * from "./types";
@@ -6,4 +7,5 @@ export * from "./utils";
 export * from "./components/NotionBlock";
 export * from "./components/CodeRenderer";
 
-export default NotionBlock;
+export { NotionBlocks, NotionBlock };
+export default NotionBlocks;
